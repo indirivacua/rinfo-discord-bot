8a6a7a0e02a093494222ea1004f1a364666fcaf8
Oscar Stanchi <indirivacua@hotmail.com>
2023-02-23 12:33:02 -0300

    testing expression tree

--- rinfo.js
+++ rinfo.js
@@ -179,67 +179,8 @@ class RInfo{
       }
       //look for code section
       if (tokens[index].type === "section" && tokens[index].value === "comenzar") {
-        let current_ident_level = 2
         do {
-          //look for variables operations
-          //FIXME only works with numerical operations
-          //if exist and is a variable name and
-          //TODO check for ident level
-          if (tokens[index+1] && tokens[index+1].type === "keyword_custom"){
-            const varName = tokens[index+1].value
-            //if var was defined in previous section
-            if (varName in vars){
-              //if it is an assigment of the variable
-              if (tokens[index+2] && tokens[index+2].type === "operator" && tokens[index+2].value === ":="){
-                let result = ""
-                index += 2 //keyword_custom + operator
-                do {
-                  if (tokens[index+1]) {
-                    result += tokens[index+1].value
-                  }
-                  // console.log(tokens[index+1].value, result, varName)
-                  index++
-                //until no more operands lefts
-                } while ((tokens[index+1] && tokens[index+1].value !== "fin" && tokens[index+1].ident_level === undefined)
-                //|| (tokens[index+1].type === "keyword_custom" && tokens[index+1].ident_level === 0) //case varName+imm+varName2
-                || (tokens[index+1].type === "keyword_custom" && tokens[index].type === "operator"))  //case varName1+varName2
-                // console.log(tokens[index+1], result)
-                //assign the value extracted from the result expression to the variable
-                try {
-                  vars[varName] = eval(result)
-                } catch (e) {
-                  //expression result contains variables
-                  if (e instanceof ReferenceError) {
-                    const extractWords = str => str.match(/[a-zA-Z]+/g);
-                    let varNamesInExpression = extractWords(result)
-                    varNamesInExpression = [...new Set(varNamesInExpression)]; //remove repeted elements
-                    // console.log(varNamesInExpression)
-                    varNamesInExpression.forEach((varName) => {
-                      //FIXME if an expression its like `veces+veces` will return `vars['vars['veces']']+veces`
-                      //   POSSIBLE SOLUTION: MAKE A SET OF VARNAMEINEXPRESSION AND REPLACE ALL OCURRENCE IN RESULT
-                      result = result.replaceAll(varName, `vars['${varName}']`)
-                    })
-                    // console.log(result)
-                    vars[varName] = eval(result)
-                    if (isNaN(vars[varName])) {
-                      return console.log(`Expression ${result} contains syntax errors`) //case some var not declared in result expression
-                    }
-                  } else if (e instanceof SyntaxError) {
-                    // console.log(e)
-                    return console.log(`Expression ${result} contains syntax errors`)   //case something like this `5 ++ 7` in result expression
-                  }
-                }
-              } else if (tokens[index+2]) { //this case probably is useful for `repetir varName`
-                index++
-              } /*else {
-                return console.log(`Unexpected value ${tokens[index+1].value}, expected :=`)
-              }*/
-            } else {
-              return console.log(`Variable ${varName} is undefined`)
-            }
-          } else { //line wasnt a variable
-            index++
-          }
+          //look for variables operations => make expression binary tree
           //look for repetir
           //look for normal instructions (e.g: mover, derecha)
           // console.log(tokens[index], tokens[index+1])
---
+++ tree.js
@@ -0,0 +1,180 @@
+class Node{
+  constructor(){
+    this.left = null
+    this.right = null
+    this.data = null
+  }
+  setData(data){
+    this.data = data
+  }
+  setBranches(){
+    this.left = new Tree()
+    this.right = new Tree()
+  }
+  setBranchesCustom(left, right){
+    this.left = left
+    this.right = right
+  }
+  getData(){
+    return this.data
+  }
+  getLeft(){
+    return this.left
+  }
+  getRight(){
+    return this.right
+  }
+}
+
+class Tree{
+  constructor(){
+    this.root = new Node()
+  }
+  isEmpty(){
+    return (this.root.getData()) ? false : true
+  }
+  insert(data, branch=null){
+    if (this.isEmpty()){
+      this.root.setData(data)
+      this.root.setBranches()
+    } else {
+      if (data < this.root.getData()) {
+        let node_left = this.root.getLeft()
+        node_left.insert(data, branch)
+      } else {
+        if (data > this.root.getData()) {
+          let node_right = this.root.getRight()
+          node_right.insert(data, branch)
+        }
+      }
+    }
+  }
+  inOrder(){
+    if (!this.isEmpty()){
+      this.root.getLeft().inOrder()
+      console.log(this.root.getData())
+      this.root.getRight().inOrder()
+    }
+  }
+}
+
+a = new Tree();
+console.log(a.isEmpty())
+a.insert(5);
+a.insert(7);
+a.insert(2);
+a.insert(3);
+a.insert(6);
+a.inOrder();
+
+function checkPriorty(PRIORITY, char, element){
+  let index = 0
+  let orderChar = 0
+  let oderElement = 0
+  let length = PRIORITY.length
+  // console.log(char, element)
+  while (index < length){
+    if (Array.isArray(PRIORITY[index])){
+      if (PRIORITY[index].includes(char)){
+        orderChar = index
+      }
+      if (PRIORITY[index].includes(element)){
+        oderElement = index
+      }
+    } else {
+      if (char ===PRIORITY[index])
+        orderChar = index
+      if (element ===PRIORITY[index])
+        oderElement = index
+    }
+    index++
+  }
+  return (orderChar > oderElement)
+}
+
+function infija2postfija(expression) {
+  let stack = []
+  let index = 0
+  let length = expression.length
+  let OPERATORS = ['+','-','*','/']
+  let PRIORITY = ['^', ['*','/'], ['+','-']].reverse() //orden de menor a mayor
+  let out = ''
+  let retorno = ""
+  // console.log(PRIORITY)
+  while (index < length) {
+    const char = expression[index]
+    if (OPERATORS.includes(char)){
+      if (stack.length !== 0){
+        while (stack[stack.length-1] && !checkPriorty(PRIORITY, char, stack[stack.length-1])){ //retorna true si operador tiene mayor prioridad al tope de la pila
+          // console.log(char, !checkPriorty(PRIORITY, char, stack[stack.length-1]), stack[stack.length-1])
+          out = stack.pop()
+          retorno += out
+          console.log(out)
+        }
+        stack.push(char)
+      } else {
+        stack.push(char)
+      }
+    } else if (char === "(") {
+      stack.push(char)
+    } else if (char === ")") {
+      if (stack.length !== 0) {
+        while (stack[length-1] !== "("){
+          out = stack.pop()
+          retorno += out
+          console.log(out)
+        }
+        out = stack.pop()
+        retorno += out
+        console.log(out)
+      }
+    } else { //es un operando
+      out = char
+      retorno += out
+      console.log(out)
+    }
+    index++
+  }
+  stack.forEach((el) => {
+    out = el
+    retorno += out
+    console.log(out)
+  })
+  return retorno
+}
+
+expression_postfija = infija2postfija("2+5*3+1") //253*+1+
+console.log(expression_postfija)
+
+class ExpressionTree{
+  constructor(expression_postfija){
+    let OPERATORS = ['+','-','*','/']
+    let index = 0
+    let char = ''
+    let length = expression_postfija.length
+    this.stack = []
+    let n, r, hRight, hLeft
+    while (index < length) {
+      char = expression_postfija[index]
+      if (OPERATORS.includes(char)){
+        r = new Node()
+        r.setData(char)
+        hRight = this.stack.pop()
+        hLeft = this.stack.pop()
+        r.setBranchesCustom(hLeft, hRight)
+        this.stack.push(r)
+      } else { // es un operando
+        n = new Node()
+        n.setData(char)
+        this.stack.push(n)
+      }
+      index++
+    }
+  }
+  getStack(){
+    return this.stack
+  }
+}
+
+et = new ExpressionTree(expression_postfija)
+console.log(et)
